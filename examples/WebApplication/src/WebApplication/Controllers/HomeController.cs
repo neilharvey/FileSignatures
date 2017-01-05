cd5bc3d964030d74c59335538065ef4428598d2d
Neil Harvey <neil.harvey@hotmail.co.uk>
2017-01-05 13:22:22 +0000

    Added web application example

---
+++ examples/WebApplication/src/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using FileSignatures;
+using FileSignatures.Formats;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFileFormatInspector fileFormatInspector;
+
+        public HomeController(IFileFormatInspector fileFormatInspector)
+        {
+            this.fileFormatInspector = fileFormatInspector;
+        }
+
+        [HttpGet]
+        public IActionResult Index(UploadResultModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file)
+        {
+            FileFormat format;
+
+            using(var stream = file.OpenReadStream())
+            {
+                format = fileFormatInspector.DetermineFileFormat(stream);
+            }
+
+            var model = new UploadResultModel()
+            {
+                Accepted = format is Image,
+                MediaType = format?.MediaType,
+                FileName = Path.GetFileName(file.FileName)
+            };
+
+            return RedirectToAction(nameof(Index), model);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ examples/WebApplication/src/WebApplication/UploadResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    public class UploadResultModel
+    {
+        public bool? Accepted { get; set; }
+
+        public string FileName { get; set; }
+
+        public string MediaType { get; set; }
+    }
+}
