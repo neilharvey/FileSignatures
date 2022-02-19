1075d2f7269c57d28839df9cdf60d51c7fbfe2a0
Neil Harvey <neil.harvey@hotmail.co.uk>
2022-02-19 23:25:41 +0000

    Remove video format because some subclasses could be audio

--- src/FileSignatures/Formats/Isobmff.cs
+++ src/FileSignatures/Formats/Isobmff.cs
@@ -6,7 +6,7 @@ namespace FileSignatures.Formats
     /// Specifies the format of a ISO/IEC base media file format
     /// </summary>
     /// <see href="https://en.wikipedia.org/wiki/ISO/IEC_base_media_file_format"/>
-    public abstract class Isobmff : Video
+    public abstract class Isobmff : FileFormat
     {
         private static readonly byte[] signature = { 0x66, 0x74, 0x79, 0x70 };
 
