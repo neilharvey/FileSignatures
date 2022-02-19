388222c99b2ab0dc7c39d7dac4420b2bb865fdcf
Neil Harvey <neil.harvey@hotmail.co.uk>
2022-02-19 22:25:01 +0000

    Renamed 3GPP format

--- src/FileSignatures/Formats/3GPP.cs
+++ src/FileSignatures/Formats/3GPP.cs
@@ -3,11 +3,11 @@ namespace FileSignatures.Formats
     /// <summary>
     /// Specifies the format of a 3rd Generation Partnership Project 3GPP multimedia files (3GG, 3GP, 3G2)
     /// </summary>
-    public class Ftyp3Gp : Ftyp
+    public class ThreeGPP : Ftyp
     {
         private static readonly byte[] signature = { 0x33, 0x67, 0x70 };
 
-        public Ftyp3Gp() : base(signature, "video/3gpp", "3gp")
+        public ThreeGPP() : base(signature, "video/3gpp", "3gp")
         {
         }
     }
