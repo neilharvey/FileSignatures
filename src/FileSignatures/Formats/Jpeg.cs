7344865986cbd2df98c6aafca2dd2e4072094ee8
Neil Harvey <neil.harvey@hotmail.co.uk>
2022-02-19 20:04:57 +0000

    Revert "Refactor JPEG format"
    This reverts commit c4c47532cbb93a96309c8d8234db85d67be1ab9d.

--- src/FileSignatures/Formats/Jpeg.cs
+++ src/FileSignatures/Formats/Jpeg.cs
@@ -1,18 +1,18 @@
-﻿namespace FileSignatures.Formats
+namespace FileSignatures.Formats
 {
     /// <summary>
     /// Specifies the format of a Joint Photographics Experts Group (JPEG) image.
     /// </summary>
     public class Jpeg : Image
     {
-        private static readonly byte[] Soi = { 0xFF, 0xD8 };
-        private const string JpegMediaType = "image/jpeg";
-        private const string JpegExtension = "jpg";
+        private static readonly byte[] soi = new byte[] { 0xFF, 0xD8 };
+        private const string mediaType = "image/jpeg";
+        private const string extension = "jpg";
 
         /// <summary>
         /// Initialises a new Jpeg format.
         /// </summary>
-        public Jpeg() : base(Soi, JpegMediaType, JpegExtension)
+        public Jpeg() : base(soi, mediaType, extension)
         {
         }
 
@@ -20,8 +20,7 @@
         /// Initialises a new Jpeg format with the specified application marker.
         /// </summary>
         /// <param name="marker">The 2-byte application marker used by the JPEG format.</param>
-        protected Jpeg(byte[] marker)
-            : base(new[] { Soi[0], Soi[1], marker[0], marker[1] }, JpegMediaType, JpegExtension)
+        protected Jpeg(byte[] marker) : base(new byte[] { soi[0], soi[1], marker[0], marker[1] }, mediaType, extension)
         {
         }
     }
