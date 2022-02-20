c60124795f6337fe3fe4b6e402fdd44a716d0f6e
Neil Harvey <neil.harvey@hotmail.co.uk>
2022-02-20 19:56:34 +0000

    Fix incorrect mime type of m4a

--- src/FileSignatures/Formats/M4A.cs
+++ src/FileSignatures/Formats/M4A.cs
@@ -5,7 +5,7 @@ namespace FileSignatures.Formats
     /// </summary>
     public class M4A : Isobmff
     {
-        public M4A() : base(new byte[] { 0x4D, 0x34, 0x41, 0x20 }, "video/mp4", "m4a")
+        public M4A() : base(new byte[] { 0x4D, 0x34, 0x41, 0x20 }, "audio/mp4", "m4a")
         {
         }
     }
