d2a7a7d953c8f9dfbfc43301bf398af1eb491f48
Tony Qu <tonyqus@gmail.com>
2024-12-15 21:40:27 +0800

    use collection expression instead

--- src/FileSignatures/Formats/SevenZip.cs
+++ src/FileSignatures/Formats/SevenZip.cs
@@ -1,7 +1,7 @@
 namespace FileSignatures.Formats;
 
 public class SevenZip: FileFormat {
-    public SevenZip() : base(new byte[]{(byte)'7',(byte)'z',  0xbc, 0xaf,0x27, 0x1c}, 6, "application/x-7z-compressed", "7z")
+    public SevenZip() : base([(byte)'7',(byte)'z',  0xbc, 0xaf,0x27, 0x1c], 6, "application/x-7z-compressed", "7z")
     {
         
     }
