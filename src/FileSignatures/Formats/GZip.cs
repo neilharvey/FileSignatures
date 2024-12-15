6ea3da79eb7f62b6897143e6b5b6f9042a79dbef
Tony Qu <tonyqus@gmail.com>
2024-12-15 21:42:16 +0800

    remove the 3rd byte since it's about compression mode, not file signature

--- src/FileSignatures/Formats/GZip.cs
+++ src/FileSignatures/Formats/GZip.cs
@@ -2,7 +2,7 @@ namespace FileSignatures.Formats;
 
 public class GZip: FileFormat
 {
-    public GZip() : base([ 0x1f, 0x8b, 0x08],3, "application/x-gzip", "gz")
+    public GZip() : base([0x1f, 0x8b],3, "application/x-gzip", "gz")
     {
         
     }
