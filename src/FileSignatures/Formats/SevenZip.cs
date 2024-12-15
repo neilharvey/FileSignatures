28129c82d01f73ee8dada9227924dc9fc80ef215
Tony Qu <tonyqus@gmail.com>
2024-12-15 21:37:19 +0800

    fix the typo

---
+++ src/FileSignatures/Formats/SevenZip.cs
@@ -0,0 +1,8 @@
+namespace FileSignatures.Formats;
+
+public class SevenZip: FileFormat {
+    public SevenZip() : base(new byte[]{(byte)'7',(byte)'z',  0xbc, 0xaf,0x27, 0x1c}, 6, "application/x-7z-compressed", "7z")
+    {
+        
+    }
+}
