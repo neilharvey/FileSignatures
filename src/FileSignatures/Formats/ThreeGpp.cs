0a334cdcbbadda5ae983f75fdb2fedbeb1b65b5e
Neil Harvey <neil.harvey@hotmail.co.uk>
2024-12-17 07:48:05 +0000

    Rename format to match standard naming

--- src/FileSignatures/Formats/ThreeGpp.cs
+++ src/FileSignatures/Formats/ThreeGpp.cs
@@ -3,9 +3,9 @@ namespace FileSignatures.Formats
     /// <summary>
     /// Specifies the format of a 3rd Generation Partnership Project 3GPP multimedia files (3GG, 3GP, 3G2)
     /// </summary>
-    public class ThreeGPP : Isobmff
+    public class ThreeGpp : Isobmff
     {
-        public ThreeGPP() : base(new byte[] { 0x33, 0x67, 0x70 }, "video/3gpp", "3gp")
+        public ThreeGpp() : base(new byte[] { 0x33, 0x67, 0x70 }, "video/3gpp", "3gp")
         {
         }
     }
