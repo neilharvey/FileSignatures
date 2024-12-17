87971796ae868fa64d144b4c1da49dad1ae4cca2
Tony Qu <tonyqus@gmail.com>
2024-12-17 10:23:14 +0800

    revoke string signature code change

--- src/FileSignatures/Formats/RAR.cs
+++ src/FileSignatures/Formats/RAR.cs
@@ -2,7 +2,7 @@ namespace FileSignatures.Formats;
 
 public class RAR: FileFormat
 {
-    public RAR() : base("Rar!\x1A\x07\x00", 7, "application/x-rar-compressed","rar")
+    public RAR() : base([(byte)'R', (byte)'a', (byte)'r',(byte)'!', 0x1A, 0x07, 0x00], 7, "application/x-rar-compressed","rar")
     {
         
     }
