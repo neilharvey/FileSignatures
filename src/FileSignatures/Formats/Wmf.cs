f55ea68d0a4c1617588d5feaf82c57b91bf253c8
Neil Harvey <neil.harvey@hotmail.co.uk>
2024-12-17 16:40:27 +0000

    Added missing signature to Wmf

--- src/FileSignatures/Formats/Wmf.cs
+++ src/FileSignatures/Formats/Wmf.cs
@@ -1,9 +1,14 @@
 namespace FileSignatures.Formats;
 
+/// <summary>
+/// Windows Metafile format.
+/// </summary>
+/// <remarks>
+/// See https://www.iana.org/assignments/media-types/image/wmf
+/// </remarks>
 public class Wmf: FileFormat
 {
-    public Wmf() : base([], "image/wmf", "wmf")
+    public Wmf() : base([0xD7, 0xCD, 0xC6, 0x9A], "image/wmf", "wmf")
     {
-        
     }
 }
