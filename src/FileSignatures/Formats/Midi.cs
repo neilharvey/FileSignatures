77af46786f4468e10747cbc8d04a327463ae518d
Neil Harvey <neil.harvey@hotmail.co.uk>
2024-12-17 17:06:27 +0000

    Fix typo in midi media type

--- src/FileSignatures/Formats/Midi.cs
+++ src/FileSignatures/Formats/Midi.cs
@@ -1,8 +1,14 @@
 namespace FileSignatures.Formats;
 
+/// <summary>
+/// Musical Instrument Digital Interface (MIDI) format.
+/// </summary>
+/// <remarks>
+/// There does not appear to be an IANA registration for this format but audio/midi appears to be commonly used.
+/// </remarks>
 public class Midi: FileFormat
 {
-    public Midi() : base([(byte)'M',(byte)'T',(byte)'h',(byte)'d'], "audio/mime", "mid")
+    public Midi() : base([(byte)'M',(byte)'T',(byte)'h',(byte)'d'], "audio/midi", "mid")
     {
     }
 }
