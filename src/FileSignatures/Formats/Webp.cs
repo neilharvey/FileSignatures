c22c2cc0af3130c2f3af306873a435084bcf21f6
Neil Harvey <neil.harvey@hotmail.co.uk>
2024-12-20 16:28:21 +0000

    Refactored webp format

--- src/FileSignatures/Formats/Webp.cs
+++ src/FileSignatures/Formats/Webp.cs
@@ -1,13 +1,14 @@
 namespace FileSignatures.Formats
 {
     /// <summary>
-    /// Specifies the format of a Google WebP image file.
+    /// Google WebP image file format.
     /// </summary>
+    /// <remarks>
+    /// See https://www.iana.org/assignments/media-types/image/webp
+    /// </remarks>
     public class Webp : Image
     {
-        private static readonly byte[] WEBP = { 0x57, 0x45, 0x42, 0x50 };
-
-        public Webp() : base(WEBP, "image/webp", "webp", 8)
+        public Webp() : base([0x57, 0x45, 0x42, 0x50], "image/webp", "webp", 8)
         {
         }
     }
