f46dd64dfe1f88210b92f9491a3e86a65323ec54
Bart Wullems <wullemsb@gmail.com>
2025-08-13 11:44:39 +0200

    Also applied the same solution for ExcelWithMacros & PowerpointWithMacros

--- src/FileSignatures/Formats/ExcelWithMacros.cs
+++ src/FileSignatures/Formats/ExcelWithMacros.cs
@@ -1,4 +1,10 @@
-﻿namespace FileSignatures.Formats
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace FileSignatures.Formats
 {
     /// <summary>
     /// Specifies the format of an Excel workbook that supports macros.
@@ -7,6 +13,55 @@
     {
         public ExcelWithMacros() : base("xl/workbook.xml",  "application/vnd.ms-excel.sheet.macroEnabled.12", "xlsm")
         {
+        }
+
+        public bool IsMatch(IDisposable? file)
+        {
+            if (file is ZipArchive archive)
+            {
+                // Match archives which contain a non-standard version of the identifiable entry, e.g. document2.xml instead of document.xml.
+                var index = Math.Max(0, IdentifiableEntry.LastIndexOf('.'));
+                var fileName = IdentifiableEntry.Substring(0, IdentifiableEntry.Length - index);
+                var extension = IdentifiableEntry.Substring(index);
+                var matchesIdentifiableEntry = archive.Entries.Any(e => e.FullName.StartsWith(fileName, StringComparison.OrdinalIgnoreCase)
+                        && e.FullName.EndsWith(extension, StringComparison.OrdinalIgnoreCase));
+
+                var contentTypesEntry = archive.GetEntry("[Content_Types].xml");
+
+                if (contentTypesEntry == null)
+                    return false;
+
+                bool hasMacroContentType = false;
+                using (var stream = contentTypesEntry.Open())
+                {
+                    var doc = System.Xml.Linq.XDocument.Load(stream);
+                    XNamespace ns = "http://schemas.openxmlformats.org/package/2006/content-types";
+                    hasMacroContentType = doc
+                        .Descendants(ns + "Override")
+                        .Any(e =>
+                            (string?)e.Attribute("PartName") == "/xl/workbook.xml" &&
+                            (string?)e.Attribute("ContentType") == "application/vnd.ms-excel.sheet.macroEnabled.main+xml"
+                        );
+                }
+
+                return matchesIdentifiableEntry && hasMacroContentType;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IDisposable? Read(Stream stream)
+        {
+            try
+            {
+                return new ZipArchive(stream, ZipArchiveMode.Read, true);
+            }
+            catch (InvalidDataException)
+            {
+                return null;
+            }
         }
     }
 }
--- src/FileSignatures/Formats/PowerpointWithMacros.cs
+++ src/FileSignatures/Formats/PowerpointWithMacros.cs
@@ -1,12 +1,67 @@
-﻿namespace FileSignatures.Formats
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace FileSignatures.Formats
 {
     /// <summary>
     /// Specifies the format of a Powerpoint presentation that supports macros.
     /// </summary>
-    public class PowerPointWithMacros : OfficeOpenXml
+    public class PowerPointWithMacros : OfficeOpenXml, IFileFormatReader
     {
         public PowerPointWithMacros() : base("ppt/presentation.xml", "application/vnd.ms-powerpoint.presentation.macroEnabled.12", "pptm")
         {
+        }
+
+        public bool IsMatch(IDisposable? file)
+        {
+            if (file is ZipArchive archive)
+            {
+                // Match archives which contain a non-standard version of the identifiable entry, e.g. document2.xml instead of document.xml.
+                var index = Math.Max(0, IdentifiableEntry.LastIndexOf('.'));
+                var fileName = IdentifiableEntry.Substring(0, IdentifiableEntry.Length - index);
+                var extension = IdentifiableEntry.Substring(index);
+                var matchesIdentifiableEntry = archive.Entries.Any(e => e.FullName.StartsWith(fileName, StringComparison.OrdinalIgnoreCase)
+                        && e.FullName.EndsWith(extension, StringComparison.OrdinalIgnoreCase));
+
+                var contentTypesEntry = archive.GetEntry("[Content_Types].xml");
+
+                if (contentTypesEntry == null)
+                    return false;
+
+                bool hasMacroContentType = false;
+                using (var stream = contentTypesEntry.Open())
+                {
+                    var doc = System.Xml.Linq.XDocument.Load(stream);
+                    XNamespace ns = "http://schemas.openxmlformats.org/package/2006/content-types";
+                    hasMacroContentType = doc
+                        .Descendants(ns + "Override")
+                        .Any(e =>
+                            (string?)e.Attribute("PartName") == "/ppt/presentation.xml" &&
+                            (string?)e.Attribute("ContentType") == "application/vnd.ms-powerpoint.presentation.macroEnabled.main+xml"
+                        );
+                }
+
+                return matchesIdentifiableEntry && hasMacroContentType;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IDisposable? Read(Stream stream)
+        {
+            try
+            {
+                return new ZipArchive(stream, ZipArchiveMode.Read, true);
+            }
+            catch (InvalidDataException)
+            {
+                return null;
+            }
         }
     }
 }
